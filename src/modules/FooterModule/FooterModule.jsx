70846226c1c92dc48cb16dec85a994dd9f639312
klerok <klerikdeezaster@gmail.com>
2026-01-23 19:59:26 +0300

    update footer

--- src/modules/FooterModule/FooterModule.jsx
+++ src/modules/FooterModule/FooterModule.jsx
@@ -1,5 +1,17 @@
+import { Logo } from "../../components/Logo/Logo";
+import { Phone } from "../../components/Phone/Phone";
+import styles from "./styles/index.module.css";
+
 export const FooterModule = () => {
-    return (
-        <footer>footer</footer>
-    )
-}
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.footer_container}>
+        <div className={styles.footer_info}>
+          <Logo />
+          <Phone />
+        </div>
+        <p className={styles.footer_desc}>© ROBO.SCHOOL</p>
+      </div>
+    </footer>
+  );
+};
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,10 @@
+import { FooterModule } from "../../modules/FooterModule/FooterModule.jsx";
 import { HeaderModule } from "../../modules/HeaderModule/HeaderModule.jsx";
 export const HomePage = () => {
   return (
     <>
       <HeaderModule />
+      <FooterModule />
     </>
-
   );
 };
